29a4af38f54ce5b6086c8735188a3c17c22a5baf
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2021-06-21 14:43:43 -0300

    Update q01.js

--- Lista_Prof_Fabio/Lista_04/q01.js
+++ Lista_Prof_Fabio/Lista_04/q01.js
@@ -1,6 +1,3 @@
-// Leia uma lista de números e que para cada número lido, escreva o próprio número e a relação de seus
-// divisores. (flag número = 0).
-
 const input = require('prompt-sync')()
 
 function main(){
