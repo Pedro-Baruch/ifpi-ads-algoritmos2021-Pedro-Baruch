4bde7a2d70847dd08bc64c1c99af71afc5959164
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2021-07-01 20:08:15 -0300

    Falta 31

--- Lista_Prof_Fabio/Lista_04/q20.js
+++ Lista_Prof_Fabio/Lista_04/q20.js
@@ -1,11 +1,29 @@
-function main(){
+const input = require('prompt-sync')();
 
-    // Entrada
-    let distancia_percorrida = Number(input('Insira a distancia percorrida desde a ultima marcação: '))
-    let gasolina_litros = Number(input('Insira '))    
-    let distancia = 600
-    let gasolina = 50
-
-    // Processamento
+function main() {
     
+    // Entrada
+    let distancia = Number(input('Insira o valor da distancia percorrida da ultima marcação (KM): '))
+    let consumo = Number(input('Insira a quantidade de combustivel consumido (L): '))
+    
+    // Processamento
+    let distancia = 600
+    let max_combustivel = 50
+    let combustivel_gasto = distancia/consumo
+    
+    while(consumo > max_combustivel){
+        console.log('Consumo maior que 50L, isso é impossivel')
+    }
+    
+    if(distancia >= distancia){
+        console.log('Você chegou ao seu destino')
+    }else if(consumo == max_combustivel){
+        console.log('Você não chegou ao seu destino')
+    }else{
+        console.log('Você nãp chegou no seu destino mas ainda tem combustivel')
+    }
+    
+    console.log(`Consumo foi de ${combustivel_gasto} KM/L`)
 }
+
+main();
--- Lista_Prof_Fabio/Lista_04/q31.js
+++ Lista_Prof_Fabio/Lista_04/q31.js
@@ -1,5 +1,16 @@
 const input = require('prompt-sync')()
 
 function main(){
+
+    // Entrada
+    const num = Number(input('Insira um numero com no maximo 3 digitos: '))
+
+    // Processamento
+    let unidade = num % 10
+    let dezena = ((num - unidade) / 10) % 10
+    let centena = ((((num - unidade) / 10) - dezena) /10) % 10
+
     
 }
+
+main()
