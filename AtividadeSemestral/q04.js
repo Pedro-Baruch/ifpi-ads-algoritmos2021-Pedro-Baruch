7855354b8d64a5a6bd889b25ea433430ad237f49
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2021-07-12 17:21:40 -0300

    Resolução

---
+++ AtividadeSemestral/q04.js
@@ -0,0 +1,15 @@
+const input = require('prompt-sync')()
+
+function main(){
+
+    // Entrada
+    console.log('Escolha entre as 3 opções de crédito: 40 mil, 60 mil e 81 mil')
+    const credito = Number(input('Opção de crédito: '))
+    console.log('Escolha entre as 3 opções de parcelas mensais: R$ 1,000 ; R$ 1,200 ; R$ 1,500')
+    const parcela = Number(input('Opção de parcela: '))
+
+    // Processamento
+
+}
+
+function
